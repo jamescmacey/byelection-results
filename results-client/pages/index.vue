a1bfae8dc886c4988ca74e5f3270dc8a88645aea
James Macey <james@jamescmacey.com>
2022-12-10 19:05:53 +1300

    Update index.vue

--- results-client/pages/index.vue
+++ results-client/pages/index.vue
@@ -55,15 +55,15 @@
         <hr>
         <div v-if="!electionStarted">
           <div class="col-12 text-start">
-            <h5>Election results will be available here from 7pm NZST on 18 June.</h5>
+            <h5>Election results will be available here from 7pm NZDT on 10 December.</h5>
             <p>You can also view official election results at <a target="_blank" href="https://electionresults.govt.nz">electionresults.govt.nz</a>, but you'll need to keep refreshing the page for these to show. Election results on this website are provided by the Electoral Commission through their official feed, and may be subject to a slight delay; but you won't need to keep refreshing this page to update results.</p>
             <p>The Electoral Commission's targets for the release of preliminary results are:
               <ul>
                 <li>
-                  Results from 50 per cent of voting places by 9pm NZST
+                  Results from 50 per cent of voting places by 9pm NZDT
                 </li>
                 <li>
-                  Results from 95 per cent of voting places by 10.30pm NZST
+                  Results from 95 per cent of voting places by 10.30pm NZDT
                 </li>
               </ul>
             </p>
