8b7be6cc666037b95a05bb718222e12b39481359
James Macey <james@jamescmacey.com>
2023-11-25 15:15:41 +1300

    Update LoadingPage.vue

--- results-client/components/LoadingPage.vue
+++ results-client/components/LoadingPage.vue
@@ -5,15 +5,15 @@
         <hr>
         <div v-if="!electionStarted">
           <div class="col-12 text-start">
-            <h5>Election results will be available here from 7pm NZDT on 10 December.</h5>
+            <h5>Election results will be available here from 7pm NZDT on 25 November.</h5>
             <p>You can also view official election results at <a target="_blank" href="https://electionresults.govt.nz">electionresults.govt.nz</a>, but you'll need to keep refreshing the page for these to show. Election results on this website are provided by the Electoral Commission through their official feed, and may be subject to a slight delay; but you won't need to keep refreshing this page to update results.</p>
             <p>The Electoral Commission's targets for the release of preliminary results are:
               <ul>
                 <li>
-                  Results from 50 per cent of voting places by 9pm NZDT
+                  Results from 50 per cent of voting places by 21.00 NZDT
                 </li>
                 <li>
-                  Results from 95 per cent of voting places by 10.30pm NZDT
+                  Results from 95 per cent of voting places by 22.30 NZDT
                 </li>
               </ul>
             </p>
